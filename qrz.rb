f21d91d9e0c61b635e115d5f620ed8a78278c816
Brad McConahay <brad@netcrafters.com>
2010-12-25 06:42:08 -0500

    Completed basic functionality, without prerequisite method calls.

--- qrz.rb
+++ qrz.rb
@@ -2,24 +2,76 @@
 
 require 'rubygems'
 require 'open-uri'
-require 'xmlsimple' # docs at http://xml-simple.rubyforge.org/
+require 'xmlsimple'
+require 'cgi'
 
-class Qrz
+class QRZ
 
-	def get_birth(callsign)
-		callsign = "n8qq"
-		qrz_base_url = 'http://www.qrz.com/xml'
-		url = "#{qrz_base_url}/bin/xml?username=n8qq;password=bamcqrz;agent=qrz_gem_test0.0.0"
-		xml = open(url).read
-		session = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
-		key = session['Session']['Key']
-		url = "#{qrz_base_url}/bin/xml?s=#{key};callsign=#{callsign}"	
-		xml = open(url).read
-		calldata = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
-		return calldata['Callsign']['born']
+	attr_accessor :callsign, :username, :password, :key
+	attr_reader   :session, :listing, :bio, :bio_file, :dxcc
+
+	QRZ_URL = 'http://www.qrz.com/xml'
+	AGENT = 'ruby-gem-qrz-callbook-0.0.1'
+
+	def initialize(args={})
+		@callsign = args['callsign']
+		@username = args['username']
+		@password = args['password']
+		@key = args['key']
+		@callsign = args['callsign']
 	end
 
-end # class Qrz
+	def login
+		url = "#{QRZ_URL}/bin/xml?username=#{@username};password=#{@password};agent=#{AGENT}"
+		xml = open(url).read
+		content  = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
+		@session = content['Session']
+		@key = @session['Key']
+	end
 
-call = Qrz.new
-puts call.get_birth('n8qq')
+	def get_listing
+		url = "#{QRZ_URL}/bin/xml?s=#{@key};callsign=#{@callsign}";
+		xml = open(url).read
+		content = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
+		@session = content['Session']
+		@listing = content['Callsign']			
+	end
+
+	def get_bio
+		url = "#{QRZ_URL}/bin/xml?s=#{@key};bio=#{@callsign}";
+		xml = open(url).read
+		content = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
+		@session = content['Session']
+		@bio = content['Bio']
+	end
+
+	def get_bio_file
+		url = "#{@bio['bio']}";
+		content = open(url).read
+		content = content.gsub(/&nbsp;/," ").gsub(/<.*?>/m, '').gsub(%r{(\n\s*){2}}, "\n\n")
+		@bio_file = CGI.unescapeHTML(content)
+	end
+
+	def get_dxcc
+		url = "#{QRZ_URL}/bin/xml?s=#{@key};dxcc=#{@callsign}";
+		xml = open(url).read
+		content = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
+		@session = content['Session']
+		@dxcc = content['DXCC']	
+	end
+
+end # class QRZ
+
+qrz = QRZ.new({
+		'callsign' => 'n8qq',
+		'username' => 'n8qq',
+		'password' => 'bamcqrz'
+})
+
+qrz.login
+qrz.get_bio
+qrz.get_bio_file
+puts qrz.bio_file
+
+#qrz.dxcc.each_pair { |key, value| puts "#{key} = #{value}" }
+
