3d6cd5413fffd3cb71dc1ba2d3c3f84e477a020c
Brad McConahay <brad@netcrafters.com>
2010-12-25 08:02:15 -0500

    Added prerequisite method calls, and error handling.

--- qrz.rb
+++ qrz.rb
@@ -7,6 +7,8 @@ require 'cgi'
 
 class QRZ
 
+	class SessionError < StandardError; end
+
 	attr_accessor :callsign, :username, :password, :key
 	attr_reader   :session, :listing, :bio, :bio_file, :dxcc
 
@@ -18,34 +20,54 @@ class QRZ
 		@username = args['username']
 		@password = args['password']
 		@key = args['key']
-		@callsign = args['callsign']
+		raise "A QRZ API subscription username is required." if @username.to_s.empty?
+		raise "A QRZ API subscription password is required." if @password.to_s.empty?
 	end
 
 	def login
+		raise "A QRZ API subscription username is required." if @username.to_s.empty?
+		raise "A QRZ API subscription password is required." if @password.to_s.empty?
 		url = "#{QRZ_URL}/bin/xml?username=#{@username};password=#{@password};agent=#{AGENT}"
 		xml = open(url).read
 		content  = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
 		@session = content['Session']
+		raise SessionError, @session['Error'] if @session['Error']
 		@key = @session['Key']
+		raise SessionError, "Unknown Error: Could not retrieve session key." if @session['Key'].to_s.empty?
 	end
 
 	def get_listing
+		raise "An amateur radio callsign is required to retrieve data." if @callsign.to_s.empty?
+		login if @key.to_s.empty?
 		url = "#{QRZ_URL}/bin/xml?s=#{@key};callsign=#{@callsign}";
 		xml = open(url).read
 		content = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
 		@session = content['Session']
+		raise SessionError, @session['Error'] if @session['Error']
 		@listing = content['Callsign']			
 	end
 
 	def get_bio
+		raise "An amateur radio callsign is required to retrieve data." if @callsign.to_s.empty?
+		login if @key.to_s.empty?
 		url = "#{QRZ_URL}/bin/xml?s=#{@key};bio=#{@callsign}";
 		xml = open(url).read
 		content = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
 		@session = content['Session']
+		if @session['Error'] =~ /not found/i
+			@bio = {}
+			return
+		end
+		raise SessionError, @session['Error'] if @session['Error']
 		@bio = content['Bio']
 	end
 
 	def get_bio_file
+		get_bio if @bio.to_s.empty?
+		if @bio['bio'].to_s.empty?
+			@bio_file = ''
+			return;
+		end
 		url = "#{@bio['bio']}";
 		content = open(url).read
 		content = content.gsub(/&nbsp;/," ").gsub(/<.*?>/m, '').gsub(%r{(\n\s*){2}}, "\n\n")
@@ -53,25 +75,14 @@ class QRZ
 	end
 
 	def get_dxcc
+		raise "An amateur radio callsign is required to retrieve data." if @callsign.to_s.empty?
+		login if @key.to_s.empty?
 		url = "#{QRZ_URL}/bin/xml?s=#{@key};dxcc=#{@callsign}";
 		xml = open(url).read
 		content = XmlSimple.xml_in( xml, { 'ForceArray' => false } )
 		@session = content['Session']
+		raise SessionError, @session['Error'] if @session['Error']
 		@dxcc = content['DXCC']	
 	end
 
 end # class QRZ
-
-qrz = QRZ.new({
-		'callsign' => 'n8qq',
-		'username' => 'n8qq',
-		'password' => 'bamcqrz'
-})
-
-qrz.login
-qrz.get_bio
-qrz.get_bio_file
-puts qrz.bio_file
-
-#qrz.dxcc.each_pair { |key, value| puts "#{key} = #{value}" }
-
